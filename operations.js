24a7628b193db4d7548044d754117554ae780d17
Gulnara <adinadzulia@gmail.com>
2024-11-06 12:52:04 +0500

    add hw

---
+++ operations.js
@@ -0,0 +1,6 @@
+var num1 = Number(prompt("Бірінші санды іңгізіңіз")) 
+var num2 = Number(prompt("Екінші санды еңгізіңіз")) 
+var num3 = Number(prompt("Үшінші санды еңгізіңіз")) 
+var sum = 0; 
+sum = (parseInt(num1) + parseInt(num2) + parseInt(num3)) / 3;//String=>Number 
+alert("Қосындысы: " + sum)
