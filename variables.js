744aba7e445e720e8102a4c9901efebca683c638
Gulnara <adinadzulia@gmail.com>
2024-11-06 12:45:51 +0500

    add hw

---
+++ variables.js
@@ -0,0 +1,28 @@
+//Бұл адаым атындағы тип string 
+let name = "Bekarys"; 
+ 
+console.log("My name: " + name + "| Type: " + typeof name); 
+ 
+// Бұл сандағы number 
+const yerth = 16; 
+ 
+console.log("My age: " + yerth + "| Type: " + typeof yerth); 
+ 
+// Бұл жерде boolean өзгерту арқылы киінгі мән анықталған 
+let score = false 
+if (true) { 
+    let score = true 
+    console.log( "His a Student?: " + score +  "| Type: " + typeof score); 
+     
+} 
+ 
+// Бұл жерде типі string  
+var age = "Жасы: " 
+var age = "25" 
+console.log("Жасы: " + age + "| Type: " + typeof age); 
+ 
+ 
+var age = "Жасы: " 
+var age = "2" 
+var age = "5" 
+console.log("Жасы: " + "2" + "5" +  "| Type: " + typeof age);
